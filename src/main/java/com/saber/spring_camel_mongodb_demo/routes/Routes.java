a90e401adfdc572c3191dbc973f3790af84ba189
saberlee1987 <saberazizi66@yahoo.com>
2022-10-27 15:48:31 +0330

    remove extra routes

--- src/main/java/com/saber/spring_camel_mongodb_demo/routes/Routes.java
+++ src/main/java/com/saber/spring_camel_mongodb_demo/routes/Routes.java
@@ -2,8 +2,6 @@ package com.saber.spring_camel_mongodb_demo.routes;
 
 public interface Routes {
 
-	String FIND_ALL_PERSON_ROUTE_ID = "find-all-person-route-id";
-	String FIND_ALL_PERSON_ROUTE = "find-all-person-route";
 	String FIND_ALL_PERSON_ROUTE_GROUP = "find-all-person-route-group";
 	String FIND_ALL_PERSON_ROUTE_GATEWAY = "find-all-person-route-gateway";
 	String FIND_ALL_PERSON_ROUTE_GATEWAY_OUT = "find-all-person-route-gateway-out";
